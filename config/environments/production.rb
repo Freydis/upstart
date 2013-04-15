d403601e303ae4df15f02e2479066211ba18276f
Kareem Kwong <kareem.kwong@gmail.com>
2013-04-15 01:55:44 -0400

    updated application.rb and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,4 @@ Upstart::Application.configure do
 
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
-  config.assets.initialize_on_precompile = false
 end
